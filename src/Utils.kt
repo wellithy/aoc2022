89f76b043c6d02e2640ff5bb0944c89f43a2dc15
Wael Ellithy <wellithy@yahoo.com>
2022-11-30 20:53:27 -0800

    Reformat

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,13 +1,17 @@
-import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+val srcRoot = Path("src")
+fun readInput(name: String) = srcRoot.resolve("$name.txt").readLines()
 
+fun ByteArray.hex(): String = BigInteger(1, this).toString(16)
 /**
  * Converts string to md5 hash.
  */
-fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+fun ByteArray.md5(): ByteArray = MessageDigest.getInstance("MD5").digest(this)
+fun String.md5(): String = toByteArray().md5().hex()
