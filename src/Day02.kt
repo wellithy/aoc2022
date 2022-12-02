083b3b900aa7a2b4467dc14edbb2ec015152ddfd
Wael Ellithy <wellithy@yahoo.com>
2022-12-01 22:05:47 -0800

    Reformat

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,12 +1,73 @@
 fun main() {
 
-    fun part1(input: Sequence<String>): Int = 1
+    fun Sequence<String>.parse(): Sequence<Pair<Shape, Symbol>> = sequence {
+        forEach { line ->
+            yield(line.split(" ").let { Shape.lookup(it[0]) to Symbol.lookup(it[1]) })
+        }
+    }
 
-    fun part2(input: Sequence<String>): Int = 2
+    fun part1(input: Sequence<String>): Int {
+        val strategy = mapOf(Symbol.Y to Shape.Paper, Symbol.X to Shape.Rock, Symbol.Z to Shape.Scissors)
+        return input.parse().map { (other, self) ->
+            strategy.getValue(self).run { score + result(other).score }
+        }.sum()
+    }
 
-    test(::part1, 1)
-    go(::part1, 1)
+    fun part2(input: Sequence<String>): Int {
+        val strategy = mapOf(Symbol.Y to Result.Draw, Symbol.X to Result.Lost, Symbol.Z to Result.Won)
+        return input.parse().map { (other, self) ->
+            strategy.getValue(self).run { score + other.toHave(this).score }
+        }.sum()
+    }
 
-    test(::part2, 2)
-    go(::part2, 2)
+    test(::part1, 15)
+    go(::part1, 11449)
+
+    test(::part2, 12)
+    go(::part2, 13187)
+
 }
+
+
+private enum class Symbol {
+    X, Y, Z;
+
+    companion object {
+        val names = values().associateBy(Symbol::name)
+        fun lookup(str: String): Symbol = names.getValue(str)
+    }
+}
+
+private enum class Result(val score: Int) {
+    Lost(0), Draw(3), Won(6)
+}
+
+private enum class Shape(val score: Int, val sym: String) {
+    Rock(1, "A"),
+    Paper(2, "B"),
+    Scissors(3, "C"),
+    ;
+
+    companion object {
+        val names = values().associateBy(Shape::sym)
+        fun lookup(str: String): Shape = names.getValue(str)
+
+        val defatedBy = mapOf(Scissors to Rock, Rock to Paper, Paper to Scissors)
+
+        val defates = defatedBy.asSequence().map { it.value to it.key }.toMap()
+    }
+
+    fun toHave(result: Result): Shape = when (result) {
+        Result.Draw -> this
+        Result.Lost -> defates.getValue(this)
+        else -> defatedBy.getValue(this)
+    }
+
+    fun result(other: Shape): Result = when {
+        this == other -> Result.Draw
+        this == defatedBy[other] -> Result.Won
+        else -> Result.Lost
+    }
+
+}
+
