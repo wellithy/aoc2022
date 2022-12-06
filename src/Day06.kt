6d5fd4f0d361e4d483f7ddd2c6ef10224f6a9dec
Wael Ellithy <wellithy@yahoo.com>
2022-12-06 08:27:26 -0800

    Reformat

--- src/Day06.kt
+++ src/Day06.kt
@@ -2,15 +2,21 @@ package day06
 
 import util.*
 
-fun part1(input: Sequence<String>): Int = input.count()
-fun part2(input: Sequence<String>): Int = input.count()
+const val PACKET_SIZE = 4
+const val MESSAGE_SIZE = 14
+
+fun String.startOfMarker(size: Int): Int =
+    asSequence().windowed(size, 1).indexOfFirst { it.toSet().size == size } + size
+
+fun part1(input: Sequence<String>): List<Int> = input.map { it.startOfMarker(PACKET_SIZE) }.toList()
+fun part2(input: Sequence<String>): List<Int> = input.map { it.startOfMarker(MESSAGE_SIZE) }.toList()
 
 
 fun main() {
-    test(::part1, 0)
-    go(::part1, 0)
+    test(::part1, listOf(7, 5, 6, 10, 11))
+    go(::part1, listOf(1093))
 
-    test(::part2, 0)
-    go(::part2, 0)
+    test(::part2, listOf(19, 23, 23, 29, 26))
+    go(::part2, listOf(3534))
 }
 
---
+++ src/Day07.kt
@@ -0,0 +1,16 @@
+package day07
+
+import util.*
+
+fun part1(input: Sequence<String>): Int = input.count()
+fun part2(input: Sequence<String>): Int =input.count()
+
+
+fun main() {
+    test(::part1, 0)
+    go(::part1, 0)
+
+    test(::part2, 0)
+    go(::part2, 0)
+}
+
